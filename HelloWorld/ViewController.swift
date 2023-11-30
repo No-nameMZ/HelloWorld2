e7dc93e2cefc1263c1ae10dd130b9c1c1f7f52b2
No-nameMZ <sagid4@yandex.ru>
2023-11-30 18:40:40 +0300

    View Controller Code

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        greetingLabel.isHidden.toggle()
+         
+        /* isHidden - утвеждение (greetingLabel - скрыт)
+          toggle() - противоположно */
         
+        greetingButton.layer.cornerRadius = 10
+        
+        /* layer - слой, есть у каждого элемента интерфейса
+         cornerRadius = 10 - сглугление угла на 10 поинтов */
     }
 
-
+    @IBAction func greetingButtonDidTapped() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.setTitle("Show Greeting", for: <#T##UIControl.State#>)
+        /* setTitle - Установить заголовок  */
+    }
+    
 }
 
+ 
