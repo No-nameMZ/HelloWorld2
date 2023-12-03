0feaeed2fd6a9726f0e585972000d43c28f173bb
No-nameMZ <sagid4@yandex.ru>
2023-12-03 23:33:33 +0300

    Work with button configuration

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -23,14 +23,32 @@ class ViewController: UIViewController {
         
         /* layer - слой, есть у каждого элемента интерфейса
          cornerRadius = 10 - сглугление угла на 10 поинтов */
+        
+        
+        
+        greetingButton.configuration = setupButton(with: "Show Greeting")
+        
+        /* greetingButton - это класс UIButton, у этой кнопки есть свойство configuration */
+        
+        /* configuration - когфигурация кнопки и именно в ней находятся все ее настройки, которые отвечают за внешний вид системной кнопки */
+        
+        // вызываем этот метод с параметром - setupButton(with: "Show Greeting")
     }
 
     @IBAction func greetingButtonDidTapped() {
         greetingLabel.isHidden.toggle()
-        greetingButton.setTitle(
+        greetingButton.configuration = setupButton(
+            with: greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting"
+         )
+        
+        /* greetingButton.configuration = setupButton - присвоим результат
+         работы метода setupButton и будем использовать тенальный оператор
+         with: greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting"  */
+        
+        /* greetingButton.setTitle(
             greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
             for: .normal
-         )
+         ) */
         
         /* 30 = setTitle - задаем заголовок, далее задаем условие с помощью
           тернального оператора */
@@ -63,6 +81,45 @@ class ViewController: UIViewController {
         
         // 47-48 = иначе, нужно отоброзить Hide Greeting
     }
+    
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.title = title
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.702465117, green: 0.1852335334, blue: 0.1598517895, alpha: 1)
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+        return buttonConfiguration
+    }
+    
+    // 68 = приватный метод, через который, мы будем настраивать внешний вид кнопки
+     
+    /* setupButton - метод, который будет возвращать нам объект с типом UIButton.Configuration новая конфигурация для кнопки */
+    
+    // создаем свойсво buttonConfiguration = UIButton.Configuration.filled()
+    
+    /* задаем название, которое будет меняться, поэтому название будем передавать через параметр метода setupButton(with title: String) с типом String */
+    
+    /* buttonConfiguration.title = title - мы обращаемся к конфигурации нашей
+     кнопки, у конфигурации есть свойство title, мы возмем этот title из
+     параметров и передадим */
+    
+    /* buttonConfiguration.baseBackgroundColor - меняем цвет, который выберем из палитры, для этого используем #colorLiteral( и выбираем бордовый цвет */
+    
+    /* buttonConfiguration.cornerStyle = .large - сгругление, выбираем large - большое сгругление */
+    
+    // buttonConfiguration.buttonSize = .large - сделали кнопку побольше
+    
+    /* buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+     - увеличиваем шрифт.
+     
+     attributedTitle - атрибуты для заголовка
+     font - штрифт
+     UIFont - где находятся шрифты, сам штрифт менять не нужно, нам нужно
+     изменить размер, поэтому берем системный шрифт с размером
+     systemFont(ofSize: 24) и увеличиваем до 24 */
+    
+    // return buttonConfiguration - возвращаем
     
 }
 
